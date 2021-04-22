2618255387f9ce6e6838982269f883d9a226b7f2
SrikantKanekar <kanekarsrikant01@gmail.com>
2021-04-22 17:16:00 +0530

    updated datastore to beta01

--- app/src/main/java/com/example/note/framework/datasource/datastore/SettingDatastore.kt
+++ app/src/main/java/com/example/note/framework/datasource/datastore/SettingDatastore.kt
@@ -1,8 +1,10 @@
 package com.example.note.framework.datasource.datastore
 
+import android.content.Context
 import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
 import androidx.datastore.core.Serializer
-import androidx.datastore.createDataStore
+import androidx.datastore.dataStore
 import com.example.faircon.SettingPreferences
 import com.example.faircon.SettingPreferences.*
 import com.example.note.business.domain.model.Setting
@@ -10,37 +12,26 @@ import com.example.note.framework.presentation.ui.BaseApplication
 import com.google.protobuf.InvalidProtocolBufferException
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
-import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 
-class SettingDataStore(application: BaseApplication) {
+class SettingDataStore(private val application: BaseApplication) {
 
-    private val dataStore = application
-        .createDataStore(Files.SETTING_DATASTORE_FILE, SettingSerializer)
+    private val Context.settingDataStore: DataStore<SettingPreferences> by dataStore(
+        fileName = Files.SETTING_DATASTORE_FILE,
+        serializer = SettingSerializer
+    )
 
-    private val default: SettingPreferences = newBuilder()
-        .setTheme(Theme.DARK)
-        .build()
-
-    val settingFlow: Flow<Setting> = dataStore.data
-        .catch { exception ->
-            if (exception is IOException) {
-                emit(default)
-            } else {
-                throw exception
-            }
-        }
+    val settingFlow: Flow<Setting> = application.settingDataStore.data
         .map { preferences ->
             Setting(theme = preferences.theme)
         }
 
     suspend fun updateTheme(theme: Theme) {
         withContext(IO) {
-            dataStore.updateData { settingPreferences ->
+            application.settingDataStore.updateData { settingPreferences ->
                 settingPreferences.toBuilder()
                     .setTheme(theme)
                     .build()
@@ -50,13 +41,14 @@ class SettingDataStore(application: BaseApplication) {
 }
 
 object SettingSerializer : Serializer<SettingPreferences> {
+
     override val defaultValue: SettingPreferences =
         newBuilder()
             .setTheme(Theme.DARK)
             .build()
 
 
-    override fun readFrom(input: InputStream): SettingPreferences {
+    override suspend fun readFrom(input: InputStream): SettingPreferences {
         try {
             return parseFrom(input)
         } catch (exception: InvalidProtocolBufferException) {
@@ -64,7 +56,7 @@ object SettingSerializer : Serializer<SettingPreferences> {
         }
     }
 
-    override fun writeTo(t: SettingPreferences, output: OutputStream) {
+    override suspend fun writeTo(t: SettingPreferences, output: OutputStream) {
         return t.writeTo(output)
     }
 }
