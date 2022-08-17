8252879ffcbfeb0c3341fc158e667aa699f2b027
“srikant” <kaneks5@medtronic.com>
2022-08-17 19:48:28 +0530

    sync labels during startup

--- app/src/main/java/com/example/note/presentation/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/note/presentation/ui/splash/SplashViewModel.kt
@@ -5,15 +5,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.note.repository.LabelRepository
 import com.example.note.repository.NoteRepository
 import com.example.note.util.printLogD
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val noteRepository: NoteRepository
+    private val noteRepository: NoteRepository,
+    private val labelRepository: LabelRepository
 ) : ViewModel() {
 
     var synced by mutableStateOf(false)
@@ -22,7 +27,12 @@ class SplashViewModel @Inject constructor(
     init {
         viewModelScope.launch {
             try {
-                noteRepository.syncNotes()
+                withContext(IO) {
+                    listOf(
+                        launch { noteRepository.syncNotes() },
+                        launch { labelRepository.syncLabels() }
+                    ).joinAll()
+                }
             } catch (e: Exception) {
                 printLogD("SplashViewModel", e.message.toString())
             } finally {
