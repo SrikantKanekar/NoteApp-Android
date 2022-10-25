dac9b62a8c775cedc8d20afa597713b94cfabb18
srikant <raos17@medtronic.com>
2022-10-25 19:55:53 +0530

    updated compose dependencies

--- app/src/main/java/com/example/note/presentation/ui/details/components/DetailsTopAppBar.kt
+++ app/src/main/java/com/example/note/presentation/ui/details/components/DetailsTopAppBar.kt
@@ -5,10 +5,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.AddAlert
 import androidx.compose.material.icons.outlined.Archive
 import androidx.compose.material.icons.outlined.PushPin
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,7 +16,7 @@ fun DetailsTopAppBar(
     onReminderClick: () -> Unit,
     onArchiveClick: () -> Unit,
 ) {
-    SmallTopAppBar(
+    TopAppBar(
         navigationIcon = {
             IconButton(
                 onClick = onBackPressed
