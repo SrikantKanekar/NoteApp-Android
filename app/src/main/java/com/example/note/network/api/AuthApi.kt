8f223388c84871ea652519afe38d8d70decf9541
srikant <kanekarsrikant01@gmail.com>
2023-01-08 16:36:17 +0530

    fixed auth apis

--- app/src/main/java/com/example/note/network/api/AuthApi.kt
+++ app/src/main/java/com/example/note/network/api/AuthApi.kt
@@ -1,19 +1,20 @@
 package com.example.note.network.api
 
+import com.example.note.model.User
 import com.example.note.network.requests.LoginRequest
-import com.example.note.network.response.SimpleResponse
+import com.example.note.network.requests.RegisterRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthApi {
 
-    @POST("/register")
+    @POST("/auth/register")
     suspend fun register(
-        @Body registerRequest: LoginRequest
-    ): SimpleResponse
+        @Body registerRequest: RegisterRequest
+    ): User
 
-    @POST("/login")
+    @POST("/auth/login")
     suspend fun login(
         @Body loginRequest: LoginRequest
-    ): SimpleResponse
+    ): User
 }
